bc19e93a61a4b5c92867b639f2ff15b49f383375
Sidrabaz <sidrabaz6@gmail.com>
2025-01-06 09:56:52 +0500

    Add files via upload

---
+++ 1st program.cpp
@@ -0,0 +1,12 @@
+// My first program
+/* This program will illustrate*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	  cout<<"NAME "<<"SAHEEFA SABA"<<endl;
+	  cout<<"\n ROLL NO"<<"002" ;
+	  
+	  return 0;
+}
+	  
